73f3bbe03212da55ef057450a258790c62540bf9
Nelson Salles de Barros Filho <nelsonsbf@protonmail.com>
2022-01-06 11:16:00 -0300

    fix(package.json, rollup.config): fixed configurations

--- rollup.config.js
+++ rollup.config.js
@@ -2,10 +2,6 @@ import babel from 'rollup-plugin-babel';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import external from 'rollup-plugin-peer-deps-external';
-// import json from '@rollup/plugin-json';
-// import nodePolyfills from 'rollup-plugin-polyfill-node';
-
-import pkg from './package.json';
 
 const extensions = ['.js', '.jsx', '.ts', '.tsx'];
 
@@ -13,15 +9,11 @@ export default [
   {
     input: 'src/index.js',
     watch: true,
-    output: [{
-      file: pkg.main,
-      format: 'cjs',
+    output: {
+      file: 'dist/bundle.js',
+      format: 'iife',
       sourcemap: true,
-    }, {
-      file: pkg.module,
-      format: 'es',
-      sourcemap: true,
-    }],
+    },
     plugins: [
       external(),
       babel({
@@ -29,16 +21,8 @@ export default [
         presets: ['@babel/env', '@babel/preset-react'],
       }),
       resolve({ extensions }),
-      /*
-      json({
-        // exclude: ['node_modules/**'],
-        namedExports: true,
-        compact: true,
-        preferConst: true,
-      }),
-      */
       commonjs(),
-      // nodePolyfills(),
     ],
+    external: true,
   },
 ];
