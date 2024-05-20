bfd4e2f8a3f8675348ce69ba4314700768a62bd6
Nelson Salles de Barros Filho <nelsonsbf@outlook.com>
2024-05-20 16:56:23 -0300

    build(gulp): scripts

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,45 @@
-const { src, dest } = require('gulp');
+const { src, dest, series } = require('gulp');
+const { exec } = require('node:child_process');
+
+async function build(cb) {
+  await exec('clear');
+
+  await exec('npm run build', (error, stdout, stderr) => {
+    if (error) {
+      console.error('[Pure UI] Error building: ', error);
+      return;
+    }
+
+    console.log(stdout);
+    console.log(stderr);
+  });
+  cb();
+}
+
+function compileTypescript(cb) {
+  exec('npm run run:compiler', (error, stdout, stderr) => {
+    if (error) {
+      console.error('[Pure UI] Error running Typescript compiler: ', error);
+      return;
+    }
+
+    console.log(stdout);
+    console.log(stderr);
+  });
+}
 
 function copyJson(cb) {
-  return src('src/**/*.json')
+  src('src/**/*.json')
     .pipe(dest('dist/lib'));
+  console.log('[Pure UI] JSON files copied successfully');
+  cb();
 }
 
 function defaultTask(cb) {
-  copyJson();
+  series(build, compileTypescript, copyJson);
   cb();
 }
 
 exports.default = defaultTask;
+exports.build = build;
 exports.copyJson = copyJson;
